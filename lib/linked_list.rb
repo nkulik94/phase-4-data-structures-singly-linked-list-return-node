5c1166ca0e56de9cab4d28626151109502389431
Naftali Kulik <nkulik1994@gmail.com>
2022-08-03 14:56:50 -0400

    commit tree 3c21aa7d2edc760d638dbcbb72619d52a3653d47

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,31 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    list_size = 0
+    last_node = self.head
+
+    while last_node
+      list_size += 1
+      last_node = last_node.next_node
+    end
+
+    # if n < 1 || n > list_size || list_size == 0
+    #   return
+    # end
+
+    right_index = list_size - n
+
+    if right_index < 0 || right_index == list_size
+      return
+    end
+    
+    index = 0
+    right_node = self.head
+    while index < right_index
+      right_node = right_node.next_node
+      index += 1
+    end
+    right_node.value
   end
 
 end
